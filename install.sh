244abb45d183ac8e2f222d09c6face019111a90f
Jay Bendon <jaybocc2@gmail.com>
2018-03-06 20:13:27 -0800

    get neovim from testing

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 DOT_FILES=$(git ls-tree @{u}|awk '{print $4}' |egrep -v '(/|LICENSE|README|install.sh)')
 OS=$(uname |tr '[:upper:]' '[:lower:]')
 DEB_DEPS="exuberant-ctags wget tmux zsh vim git xclip zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libssl-dev build-essential htop"
-DEB_BACKPORTS_DEPS="neovim"
+DEB_BACKPORTS_DEPS=""
+DEB_TESTING_DEPS="neovim"
 OSX_DEPS="ctags wget neovim tmux zsh vim git hub"
 GO_VERSION=1.9.2
 ARCH=amd64
@@ -115,6 +116,7 @@ install_deps() {
     if [[ "${ID}" == "debian" ]];then
       sudo apt-get install ${DEB_DEPS}
       sudo apt-get -t stretch-backports install ${DEB_BACKPORTS_DEPS}
+      sudo apt-get -t testing install ${DEB_BACKPORTS_DEPS}
     fi
   fi
 
