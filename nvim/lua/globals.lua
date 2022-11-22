3f870948c8ca58bf28c4cdc5a49a214811f11773
Jay Bendon <jaybocc2@gmail.com>
2022-11-22 13:35:56 -0800

    initial keybinds

--- nvim/lua/globals.lua
+++ nvim/lua/globals.lua
@@ -1,18 +1,9 @@
-
 local function show_documentation()
-  local ft = vim.bo.filetype
-  local cword = vim.fn.expand("<cword>")
-  if vim.fn.index({ "vim", "help" }, ft) then
-    vim.fn.execute("help " .. cword)
-  elseif vim.fn["coc#rpc#ready"]() then
-    vim.fn.call("CocActionAsync", { "doHover" })
-  else
-    vim.fn.execute(vim.o.keywordprg .. " " .. cword)
-  end
+  vim.notify("TODO: show_documentation unimplemented", "info")
 end
 
 local function setup()
-  return
+  _G.show_documentation = show_documentation
 end
 
 return { setup = setup }
--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,17 +1,25 @@
-local binds = {
+local mappings = {
   normal_mode = {
-    -- nvim-tree
-    ["<C-n>"] = ":NvimTreeToggle<CR>",
+    -- ["C-"] = {
+    --   -- nvim-tree
+    --   n = { ":NvimTreeToggle<CR>", "toggle nvim-tree" },
+    --   q = { ":call QuickFixToggle()<CR>", "tottle quickfix" },
+    -- },
+    ["C-q"] = { ":call QuickFixToggle()<CR>", "tottle quickfix" },
+    ["<C-n>"] = { "<cmd>NvimTreeToggle<CR>", "toggle nvim-tree" },
+    -- leader key binds
+    ["<LEADER>"] = {
+      S = { "<cmd>SymbolsOutline<CR>", "toggle symbols outline" },
+    },
     -- new TAB
-    ["tn"] = ":tabnew<CR>",
+    ["tn"] = { "<cmd>tabnew<CR>", "open new tab" },
     -- misc
-    ["<F2>"] = ":set nonumber!<CR>:set foldcolumn=0<CR>",
+    -- ["<F2>"] = ":set nonumber!<CR>:set foldcolumn=0<CR>",
     -- whichkey
     ["wk"] = ":WhichKey<CR>",
     -- quick fix
-    ["]q"] = ":cnext<CR>",
-    ["[q"] = ":cprev<CR>",
-    ["<C-q>"] = ":call QuickFixToggle()<CR>",
+    ["]q"] = { ":cnext<CR>", "next fix" },
+    ["[q"] = { ":cprev<CR>", "previous fix" },
   },
   insert_mode = {
   },
@@ -26,62 +34,27 @@ local modes = {
   command_mode = "c",
 }
 
-local base_opts = { noremap = true, silent = true }
-local bind_opts = {
-  normal_mode = base_opts,
-  insert_mode = base_opts,
-  visual_mode = base_opts,
-  visual_block_mode = base_opts,
-  command_mode = base_opts,
-  term_mode = { silent = true },
-}
-
-local function mapkeybind(mode, lhs, rhs, opts)
-  local options = { noremap = true }
-  local override = {}
-  if opts ~= nil then
-    override = opts
+local function wk_mapping(wk, mode, map)
+  if mode == "n" then
+    wk.register(map)
+  else
+    wk.register(map, {mode = mode})
   end
+end
 
-  -- options = vim.tbl_extend('force', options, opts or {})
-  options = vim.tbl_extend("force", options, override)
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+local function mapkeys(mode_key, mode)
+  local wk = jaylib.loadpkg("which-key")
+  if wk == nil then
+    vim.notify("which-key plugin missing - keybinds are not setup!", "error")
+    return
+  end
+  wk_mapping(wk, mode, mappings[mode_key])
 end
 
 local function setup()
-  mapkeybind("n", "<C-n>", ":NvimTreeToggle<CR>")
-  mapkeybind("n", "tn", ":tabnew<CR>")
-  mapkeybind("n", "<F2>", ":set nonumber!<CR>:set foldcolumn=0<CR>")
-  mapkeybind("n", "wk", ":WhichKey<CR>")
-
-  -- CoC keybinds
-  -- mapkeybind("i", "<C-Space>", "coc#refresh()", { silent = true, expr = true })
-  -- mapkeybind(
-  --     "i",
-  --     "<CR>",
-  --     "coc#pum#visible() ? coc#_select_confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'",
-  --     { silent = true, expr = true }
-  -- )
-  -- mapkeybind(
-  --     "i",
-  --     "<TAB>",
-  --     "coc#pum#visible() ? coc#_select_confirm() : coc#expandableOrJumpable() ? \"<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])<CR>\" : v:lua.check_back_space() ? \"<TAB>\" : coc#refresh()",
-  --     { silent = true, expr = true }
-  -- )
-  mapkeybind("n", "K", ": v:lua.show_documentation() <CR>", { silent = true })
-  -- rename
-  -- mapkeybind("n", "<LEADER>rn", "<Plug>(coc-rename)")
-  -- format
-  -- mapkeybind("n", "<LEADER>f", "<Plug>(coc-format-selected)")
-  -- mapkeybind("n", "<LEADER>gd", ":CocDiagnostics<CR>")
-  -- mapkeybind('n', 'gd', '<Plug>(coc-definition)', { silent = true })
-  -- mapkeybind("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
-  -- mapkeybind("n", "gi", "<Plug>(coc-implementation)", { silent = true })
-  -- mapkeybind("n", "gr", "<Plug>(coc-references)", { silent = true })
-  -- mapkeybind("n", "<space>S", ":CocList outline<CR>", { silent = true })
-  -- mapkeybind("n", "<LEADER>S", ":AerialToggle<CR>")
-  -- mapkeybind('n', '<LEADER>S', ':SymbolsOutline<CR>')
-  -- mapkeybind('n', '<LEADER>S', ':Vista coc<CR>')
+  for mode_key, mode in pairs(modes) do
+    mapkeys(mode_key, mode)
+  end
 end
 
 return { setup = setup }
