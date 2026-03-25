bdff168cccab7059fa86461597c5eff3047f8cb9
Jay Bendon <jaybocc2@gmail.com>
2026-03-25 00:04:24 -0700

    fix: improve jdtls_bazel jar copy robustness

--- shlibs/lsp-deps.sh
+++ shlibs/lsp-deps.sh
@@ -68,7 +68,11 @@ jdtls_bazel() {
   curl -L -o "$(pwd)/${fname}.zip" "${url}" || TRACE "failed to download jdtls bazel plugins"
   unzip ${fname}.zip
   mkdir -p "${install_dir}"
-  cp "$(pwd)/plugins/*.jar" "${install_dir}/"
+  if [ -d "plugins" ]; then
+    cp plugins/*.jar "${install_dir}/"
+  else
+    cp "${fname}/plugins/*.jar" "${install_dir}/"
+  fi
   rmtmp
 }
 
